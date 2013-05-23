169a924475d1847e4ba5a0d09a8cb7c36add6b97
Mario Manno <mail@manno.name>
2013-05-23 23:23:23 +0000

    update fixtures, settings

--- config/initializers/media_backend.rb
+++ config/initializers/media_backend.rb
@@ -1,8 +1,10 @@
 MediaBackend::Application.configure do
     config.folders = { 
-      recordings_base_dir: '/srv/ftp',  
-      images_base_dir: '/srv/www/media.ccc.de/out/media',  
+      recordings_base_dir: '/srv/recordings/cdn',
+      images_base_dir: '/srv/www/cdn',
+      webgen_base_dir: '/srv/www/webgen/src/browse'
     }
+
     config.mime_type_folder_mappings = {
       'audio/mp3' => 'mp3',
       'video/mp4' => 'mp4',
