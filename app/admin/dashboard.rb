e316ef6c7d25fdcb5237ad08109abe6d2f969cc2
Mario Manno <mail@manno.name>
2013-05-23 23:23:23 +0000

    add recent conferences to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,18 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    columns do
+      column do
+        panel "Recent Conferences" do
+          ul do
+            Conference.recent(5).map do |conference|
+              li link_to(conference.acronym, admin_conference_path(conference))
+            end
+          end
+        end
       end
-    end
+    end # columns
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,2 +1,4 @@
 class Conference < ActiveRecord::Base
+
+  scope :recent, lambda { |n| order('created_at desc').limit(n) }
 end
