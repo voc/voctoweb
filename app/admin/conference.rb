3cd49189cb645ed4abf0c3af3314569af3c8275e
Mario Manno <mail@manno.name>
2013-05-23 23:23:23 +0000

    add conference active admin configuration

--- app/admin/conference.rb
+++ app/admin/conference.rb
@@ -1,3 +1,33 @@
 ActiveAdmin.register Conference do
 
+  index do
+    column :acronym
+    column :recordings_path
+    column :images_path
+    column :webgen_location
+    column :created_at
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "Conference Details" do
+      f.input :acronym
+      f.input :recordings_path
+      f.input :images_path
+      f.input :webgen_location
+      f.input :aspect_ratio 
+    end
+    f.actions
+  end
+
+  controller do
+    def permitted_params
+      params.permit conference: [ :acronym, :recordings_path,
+                                  :images_path, :webgen_location,
+                                  :aspect_ratio ]
+    end
+  end
+
+
+
 end
