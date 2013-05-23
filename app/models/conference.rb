2e6b66cbaf3f3226ed5ca75e2185cb7cdacc69af
Mario Manno <mail@manno.name>
2013-05-23 23:23:23 +0000

    work on model validations, active admin

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,9 +1,12 @@
 class Conference < ActiveRecord::Base
   include Recent
 
-  has_many :events,  dependent: :destroy
+  has_many :events, dependent: :destroy
 
-  def to_s
-    self.acronym
+  validates_presence_of :acronym
+
+  def display_name
+    self.acronym || self.id
   end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,13 @@
 class Event < ActiveRecord::Base
 
   belongs_to :conference
-  has_many :recordings,  dependent: :destroy
+  has_many :recordings, dependent: :destroy
 
   validates_presence_of :conference
+  validates_presence_of :guid
 
-  def to_s
-    self.guid
+  def display_name
+    self.guid.nil? ? self.id : self.guid
   end
 
 end
