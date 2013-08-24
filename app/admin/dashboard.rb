06591039fabf08e731cb14f504fe4d56a74746ef
Mario Manno <mail@manno.name>
2013-08-24 15:24:15 +0200

    add api usage example to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,12 +6,25 @@ ActiveAdmin.register_page "Dashboard" do
 
     columns do
       column do
+
         panel "Recent Conferences" do
           ul do
             Conference.recent(5).map do |conference|
               li link_to(conference.acronym, admin_conference_path(conference))
             end
           end
+        end
+
+        panel "API Examples" do
+          para "You can use the API to register a new conference. The conference `acronym` and the URL of the `schedule.xml` are required."
+
+          pre I18n.t("media-backend.conference-api-curl")
+
+          para "You can add images to an event, like the animated gif thumb and the poster image. The event is identified by its `guid` and the conference `acronym`."
+          pre I18n.t("media-backend.event-api-curl")
+
+          para "Recordings are added by specifiying the parent events `guid`, an URL and a `filename`."
+          pre I18n.t("media-backend.recording-api-curl")
         end
       end
     end # columns
