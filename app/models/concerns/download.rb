dcc7e68ad49cd58bff5b322499cc3d155dfec942
Mario Manno <mail@manno.name>
2013-12-29 18:39:59 +0100

    add error message to uploader (stacktrace in dj?)

--- app/models/concerns/download.rb
+++ app/models/concerns/download.rb
@@ -19,15 +19,23 @@ module Download
         result = FileUtils.mv uri.path, path
         Rails.logger.info "Moved to #{path}" if result == 0
       else
-        File.open(path, 'wb') do |f|
-          result = download_io(f, uri)
-        end
-        Rails.logger.info "Downloaded to #{path}" if result
+        result = download_url_to_file uri, path
       end
       result
     end
 
     private
+
+    def download_url_to_file(uri, path)
+      File.open(path, 'wb') do |f|
+        result = download_io(f, uri)
+      end
+      Rails.logger.info "Downloaded to #{path}" if result
+      result
+    rescue
+      Rails.logger.error "Failed download of #{uri} to #{path}: #{$!}"
+      return
+    end
 
     def download_io(fileio, uri)
       request = Net::HTTP::Get.new uri
