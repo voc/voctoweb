be957116f23f8211380b84647e1515f621d9f0dd
Mario Manno <mail@manno.name>
2014-05-30 20:40:19 +0200

    make test pass if database is empty

--- test/controllers/public/mirrors_controller_test.rb
+++ test/controllers/public/mirrors_controller_test.rb
@@ -4,6 +4,6 @@ class Public::MirrorsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index, format: :json
     assert_response :success
-    refute_empty JSON.parse(response.body)
+    assert JSON.parse(response.body)
   end
 end
