8fdd8bafa64603cc610cebbab8163377b7e04e94
Mario Manno <mail@manno.name>
2014-06-22 03:09:35 +0200

    fix migration for postgresql

--- db/migrate/20140615151738_change_conference_schedule_xml_to_text.rb
+++ db/migrate/20140615151738_change_conference_schedule_xml_to_text.rb
@@ -1,6 +1,6 @@
 class ChangeConferenceScheduleXmlToText < ActiveRecord::Migration
   def up
-    change_column :conferences, :schedule_xml, :text, limit: 4294967295
+    change_column :conferences, :schedule_xml, :text, limit: 10.megabyte
   end
 
   def down
