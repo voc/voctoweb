7022c5b425d49ce870bea903ee0dfb3550054307
Mario Manno <mail@manno.name>
2014-08-23 18:55:09 +0200

    add nfiles count to json

--- app/models/mirror.rb
+++ app/models/mirror.rb
@@ -6,9 +6,13 @@ end
 class Mirror < MirrorBrain
   self.table_name = :server
 
+  def nfiles
+    Mirror.connection.execute(%{SELECT mirr_get_nfiles('#{self.identifier}')}).first['mirr_get_nfiles']
+  end
+
   def to_json
     Jbuilder.encode do |json|
-      json.extract! self, :identifier, :baseurl, :baseurl_ftp, :enabled, :status_baseurl, :region, :country, :asn, :lat, :lng, :prefix, :last_scan
+      json.extract! self, :identifier, :baseurl, :baseurl_ftp, :enabled, :status_baseurl, :region, :country, :asn, :lat, :lng, :prefix, :last_scan, :nfiles
       json.baseurl_rsync baseurl_rsync unless baseurl_rsync.include? '@'
     end
   end
