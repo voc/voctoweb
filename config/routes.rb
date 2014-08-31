a56ff964306450d963ace35b4785d83716d7a03f
Mario Manno <mail@manno.name>
2014-08-31 18:25:23 +0200

    make download api calls use post

--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,13 @@ MediaBackend::Application.routes.draw do
     end
     resources :events, :defaults => { :format => 'json' } do
       collection do
-        get 'download'
+        post 'download'
         post 'update_promoted'
       end
     end
     resources :recordings, :defaults => { :format => 'json' } do
       collection do
-        get 'download'
+        post 'download'
       end
     end
     resources :news, :defaults => { :format => 'json' }
@@ -39,43 +39,4 @@ MediaBackend::Application.routes.draw do
     end
   end
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
