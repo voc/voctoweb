2fbc479b4277b7f7b924e27c1b3cec9e5633ab4d
Mario Manno <mail@manno.name>
2014-09-06 18:35:15 +0200

    add frontend url to public api event json

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -1,5 +1,9 @@
 module ViewHelper
 
+  def frontend_link(event)
+    [MediaBackend::Application.config.frontendURL, 'browse', event.conference.webgen_location, event.slug].join('/') + '.html'
+  end
+
   def show_logo_url(conference)
     return nil if conference.logo.nil?
     "(#{conference.get_logo_url})"
--- app/views/public/shared/_event.json.jbuilder
+++ app/views/public/shared/_event.json.jbuilder
@@ -4,3 +4,4 @@ json.thumb_url event.get_thumb_url
 json.gif_url event.get_gif_url
 json.poster_url event.get_poster_url
 json.url public_event_url(event, format: :json)
+json.frontend_link frontend_link(event)
