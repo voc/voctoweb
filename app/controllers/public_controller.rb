c2ea2e3a370251c1fb3d356dc7aaef11dac105dc
Mario Manno <mail@manno.name>
2015-01-03 00:11:01 +0100

    add xml support to oembed controller

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,20 +1,36 @@
 class PublicController < ActionController::Base
-  respond_to :json
   def index
+    respond_with :json
   end
 
   def oembed
+    request.format = :json unless %w[json xml].include? request.format
     @url = params[:url]
-    return render json: { status: :error } unless @url
+    respond_to do |format|
+      if parse_url(@url)
+        format.json
+        format.xml
+      else
+        format.json { render json: { status: :error } }
+        format.xml { render xml: { status: :error } }
+      end
+    end
+  end
+
+  private
+
+  def parse_url(url)
+    return unless @url
     m = @url.match(%r'\Ahttp://media.ccc.de/browse/(.*)/(.*).html\z')
-    return render json: { status: :error } unless m
+    return unless m
     @event = Event.by_identifier(m[1], m[2])
-    return render json: { status: :error } unless @event
+    return unless @event
 
     recording = @event.recordings.downloaded.video.first
-    @width = recording.width
-    @height = recording.height
-    @width = [1280, @width, params[:maxwidth].to_i].min if params[:maxwidth]
-    @height = [720, @height, params[:maxheight].to_i].min if params[:maxheight]
+    @width = [1280, recording.width.to_i].min
+    @height = [720, recording.height.to_i].min
+    @width = [@width.to_i, params[:maxwidth].to_i].min if params[:maxwidth]
+    @height = [@height.to_i, params[:maxheight].to_i].min if params[:maxheight]
+    true
   end
 end
---
+++ app/views/public/oembed.xml.builder
@@ -0,0 +1,12 @@
+xml.oembed do
+  xml.version '1.0'
+  xml.type 'video'
+  xml.provider_name 'media.ccc.de'
+  xml.provider_url 'http://media.ccc.de'
+  xml.width @width
+  xml.height @height
+  xml.title @event.title
+  xml.author @event.persons_text
+  xml.thumbnail_url @event.get_thumb_url
+  xml.html render(partial: 'html5player', formats: [:html], locals: { width: @width, height: @height, recording: @recording })
+end
