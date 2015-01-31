271d0766ab2acb9f5817fe6a96498cea9c20cebf
Mario Manno <mail@manno.name>
2015-01-31 15:51:50 +0100

    remove index from torrent whitelist, because it changes all the time

--- app/controllers/public/torrents_controller.rb
+++ app/controllers/public/torrents_controller.rb
@@ -1,6 +1,7 @@
 class Public::TorrentsController < ApplicationController
   def index
     @torrent_hashes = MirrorFile.torrent_hashes
+    @torrent_hashes.reject! { |torrent| torrent['path'].starts_with? 'INDEX' }
     respond_to do |format|
       format.text
     end
