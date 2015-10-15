ee582d53155ec81e7e7dc0c61a8a4e74e89234a2
Mario Manno <mail@manno.name>
2015-10-15 22:01:40 +0200

    add navbar dropdown menu for feeds

--- app/models/frontend/conference.rb
+++ app/models/frontend/conference.rb
@@ -9,14 +9,6 @@ module Frontend
       }
     end
 
-    def preferred_mime_type
-      available = Recording.recorded_at(self).pluck(:mime_type).uniq
-      MimeType::PREFERRED_VIDEO.each { |mime_type|
-        return MimeType.mime_type_slug(mime_type) if available.include? mime_type
-      }
-      return MimeType.mime_type_slug(available.first)
-    end
-
     def recordings_url
       File.join Settings.cdnURL, self.recordings_path
     end
