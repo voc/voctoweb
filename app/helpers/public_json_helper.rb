8863ec6eb814feeafd21411b12b16585b1f3670f
Mario Manno <mail@manno.name>
2015-10-24 19:59:19 +0200

    use frontend url in public json api response

--- app/helpers/public_json_helper.rb
+++ app/helpers/public_json_helper.rb
@@ -1,4 +1,9 @@
 module PublicJsonHelper
+  def frontend_event_url(slug: '')
+    return event_url(slug: slug) unless Rails.env.production?
+    event_url(slug: slug, host: Settings.frontend_host, protocol: Settings.frontend_proto, port: nil)
+  end
+
   def json_cached_key(identifier, *models)
     'js_' + identifier.to_s + Digest::SHA1.hexdigest(models.flatten.map { |m| m.updated_at.to_i }.join(';'))
   end
