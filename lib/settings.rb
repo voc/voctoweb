d601b3a0dff22cb519d892d618488aab6bdb48c0
Mario Manno <mail@manno.name>
2015-10-24 19:50:48 +0200

    use rails 4.2 config yml mechanism

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,25 +1,18 @@
 class Settings
   def self.method_missing(name)
     fail "not implemented: #{name}" unless config.respond_to?(name)
-    # FIXME mock this instead
-    if name == :folders && Rails.env.test?
-      {
-        'recordings_base_dir' => Rails.root.join('tmp', 'tests', 'rec'),
-        'images_base_dir' => Rails.root.join('tmp', 'tests', 'img'),
-        'recordings_webroot' => '',
-        'images_webroot' => '',
-        'tmp_dir' => '/tmp'
-      }
-    else
-      config.public_send(name)
-    end
+    config.public_send(name)
   end
 
   def self.respond_to?(name)
     config.respond_to?(name)
   end
 
+  def self.frontend_url
+    "#{frontend_proto}://#{frontend_host}"
+  end
+
   def self.config
-    @config ||= OpenStruct.new YAML.load(open(Rails.root.join('config/settings.yml')))
+    @config ||= OpenStruct.new Rails.application.config_for(:settings)
   end
 end
