962c28c1c1a4d7fb4434de2176a3dbc823cf9c03
Mario Manno <mma@i22.de>
2015-10-27 17:13:12 +0100

    add indexes on event and recordings

---
+++ db/migrate/20151027160631_add_indexes.rb
@@ -0,0 +1,8 @@
+class AddIndexes < ActiveRecord::Migration
+  def change
+    add_index 'events', ['slug'], name: 'index_events_on_slug'
+    add_index 'events', ['release_date'], name: 'index_events_on_release_date'
+    add_index 'events', %w(slug id), name: 'index_events_on_slug_and_id'
+    add_index 'recordings', %w(state mime_type), name: 'index_recordings_on_state_and_mime_type'
+  end
+end
