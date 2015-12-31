56d6877124c55884b72aa244b461f45953a98c98
Mario Manno <mail@manno.name>
2015-12-31 05:45:42 +0100

    configure puma worker killer gem in puma.rb because daemon

--- config/initializers/puma_worker_killer.rb
+++
@@ -1,7 +0,0 @@
-PumaWorkerKiller.config do |config|
-  config.ram           = 1024 * 5  # mb
-  config.frequency     = 60     # seconds
-  config.percent_usage = 0.95
-  config.rolling_restart_frequency = false
-end
-PumaWorkerKiller.start
