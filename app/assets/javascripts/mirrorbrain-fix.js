8bb9c38083ee8a7b8e1fe366e25930bf39cf52b8
Mario Manno <mail@manno.name>
2016-01-04 00:40:38 +0100

    move videoplayer javascript into assets

--- app/assets/javascripts/mirrorbrain-fix.js
+++ app/assets/javascripts/mirrorbrain-fix.js
@@ -1,20 +1,67 @@
-function selectMirror(url, cb) {
-  // always request cdn via https
-  url = url.replace(/^http/, 'https') + '.meta4';
-  console.log('asking cdn for mirror at', url);
-  return $.ajax({
-    url: url,
-    dataType: 'xml',
-    success: function(dom) {
-      var
-      $dom = $(dom),
-      $urls = $dom.find('file url'),
-      $url = $($urls.get(0)),
-      mirror = $url.text();
+var MirrorbrainFix = {
 
-      console.log('using mirror', mirror);
-      cb(mirror);
-    }
-  });
+  selectMirror: function(url, cb) {
+    // always request cdn via https
+    url = url.replace(/^http/, 'https') + '.meta4';
+    console.log('asking cdn for mirror at', url);
+    return $.ajax({
+      url: url,
+      dataType: 'xml',
+      success: function(dom) {
+        var
+        $dom = $(dom),
+        $urls = $dom.find('file url'),
+        $url = $($urls.get(0)),
+        mirror = $url.text();
+
+        console.log('using mirror', mirror);
+        cb(mirror);
+      }
+    });
+  },
+
+  setupPlayer: function() {
+    var stamp = window.location.hash.split('&t=')[1],
+        $video = $('video'),
+        promises = [];
+
+    $('video source').each(function() {
+      var $source = $(this);
+      // prop always presents the fully resolved url
+      promises.push(
+        MirrorbrainFix.selectMirror($source.prop('src'), function(mirror) {
+        $source.attr('src', mirror);
+      })
+      );
+    });
+
+    $.when.apply($, promises).done(function() {
+      $('video').mediaelementplayer({
+        usePluginFullScreen: true,
+        enableAutosize: true,
+        success: function (mediaElement) {
+          mediaElement.addEventListener('canplay', function () {
+            if(stamp) {
+              mediaElement.setCurrentTime(stamp);
+              stamp = null;
+            }
+          });
+          mediaElement.addEventListener('playing', function () {
+            $.post("/public/recordings/count", {event_id: $video.data('id'), src: mediaElement.src});
+          }, false);
+          mediaElement.addEventListener('pause', function() {
+            var hash = '#video&t='+Math.round(mediaElement.currentTime);;
+            if(window.history && window.history.replaceState) {
+              // set new hash without adding an entry into the browser history
+              window.history.replaceState(null, "", hash);
+            }
+            else {
+              // classic fallback
+              window.location.hash = hash;
+            }
+          }, false);
+        }
+      });
+    });
+  }
 }
-
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'me
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [/\.(?:eot|otf|svg|ttf|woff|swf|svg|gif|png)/]
-Rails.application.config.assets.precompile += %w( mediaelement-and-player.js mediaelement-fix.js )
+Rails.application.config.assets.precompile += %w( mediaelement-and-player.js mediaelement-fix.js mirrorbrain-fix jquery mediaelementplayer )
