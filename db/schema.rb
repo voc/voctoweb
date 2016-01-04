ba2967f6a1988d5e41ca87a90cf4ab1b38a997eb
Mario Manno <mail@manno.name>
2016-01-04 14:07:00 +0100

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102232606) do
+ActiveRecord::Schema.define(version: 20160104122727) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
