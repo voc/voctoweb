9bd17367c871f8228628885e5cc1048676d1035d
Mario Manno <mail@manno.name>
2016-04-23 15:19:06 +0200

    adapt old es query

--- app/models/concerns/elasticsearch_event.rb
+++ app/models/concerns/elasticsearch_event.rb
@@ -4,7 +4,7 @@ module ElasticsearchEvent
   extend ActiveSupport::Concern
 
   class_methods do
-    def query(str)
+    def query(term)
       search_for query: {
         function_score:  {
           query:  {
@@ -13,30 +13,30 @@ module ElasticsearchEvent
               should:  [
                 {
                   multi_match:  {
-                    query:  str,
+                    query:  term,
                     fields:  [
-                      'event.title^4',
-                      'event.subtitle^3',
-                      'event.persons^3',
+                      'title^4',
+                      'subtitle^3',
+                      'persons^3',
                       'conference.acronym^2',
                       'conference.title^2',
-                      'event.description^1'
+                      'description^1'
                     ],
                     type:  'best_fields',
                     operator:  'and',
                     fuzziness:  1
-                  },
+                  }
                 },
-                { prefix:  { 'event.title' => { value:  str, boost:  12 } } },
-                { prefix:  { 'event.subtitle' => { value:  str, boost:  3 } } },
-                { prefix:  { 'conference.acronym' => { value:  str, boost:  2 } } },
-                { prefix:  { 'conference.persons' => { value:  str, boost:  1 } } }
+                { prefix:  { 'title' => { value:  term, boost:  12 } } },
+                { prefix:  { 'subtitle' => { value:  term, boost:  3 } } },
+                { prefix:  { 'conference.acronym' => { value:  term, boost:  2 } } },
+                { prefix:  { 'conference.persons' => { value:  term, boost:  1 } } }
               ]
             }
           },
           boost:  1.2,
           functions:  [
-            { gauss:  { "event.date" =>  { scale:  "96w", decay:  0.5 } } }
+            { gauss:  { date:  { scale:  '96w', decay:  0.5 } } }
           ]
         }
       }
@@ -52,17 +52,11 @@ module ElasticsearchEvent
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
 
-    settings index: { number_of_shards: 1 } do
-      mappings dynamic: 'false' do
-        indexes :title, analyzer: 'english', index_options: 'offsets'
-      end
-    end
-
     def as_indexed_json(_options = {})
       as_json(
-        only: %i(frontend_link guid thumb_url release_date date description poster_url link title url length persons subtitle updated_at),
+        only: %i(title subtitle description persons length release_date date updated_at),
         id: :guid,
-        include: { conference: { only: %i(title acronym frontend_link logo url) }
+        include: { conference: { only: %i(title acronym) }
       })
     end
   end
