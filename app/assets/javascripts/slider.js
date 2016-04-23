6aa533083ef104e5f14e563b451e374538d0e7e1
MaZderMind <git@mazdermind.de>
2016-04-23 21:41:36 +0200

    also register slider on turbolinks event

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,15 +1,19 @@
 $(function() {
-  $('.promoted .slider').bxSlider({
-    slideWidth: 200,
-    minSlides: 1,
-    maxSlides: 5,
-    slideMargin: 35,
+  function init() {
+    $('.promoted .slider').bxSlider({
+      slideWidth: 200,
+      minSlides: 1,
+      maxSlides: 5,
+      slideMargin: 35,
 
-    captions: true,
+      captions: true,
 
-    auto: true,      // Slides will automatically transition
-    pause: 3000,     // The amount of time (in ms) between each auto transition
-    autoHover: true, // Auto show will pause when mouse hovers over slider
-    moveSlides: 1    // The number of slides to move on transition.
-  });
+      auto: true,      // Slides will automatically transition
+      pause: 3000,     // The amount of time (in ms) between each auto transition
+      autoHover: true, // Auto show will pause when mouse hovers over slider
+      moveSlides: 1    // The number of slides to move on transition.
+    });
+  }
+  init();
+  $(document).on('page:load', init);
 });
