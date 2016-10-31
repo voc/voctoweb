0b474d1a8abb6bc5c22c2a67ada3e7d5f2a4f49b
Mario Manno <mail@manno.name>
2016-10-31 12:17:03 +0100

    Clean up two overwriting method warnings

--- app/models/frontend/recording.rb
+++ app/models/frontend/recording.rb
@@ -2,8 +2,6 @@ module Frontend
   class Recording < ::Recording
     belongs_to :event, class_name: Frontend::Event
     scope :by_mime_type, ->(mime_type) { where(mime_type: mime_type) }
-    scope :audio, -> { where(mime_type: MimeType::AUDIO) }
-    scope :subtitle, -> { where(mime_type: MimeType::SUBTITLE) }
 
     def url
       File.join(event.conference.recordings_url, folder || '', filename).freeze
