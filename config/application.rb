d8675a7d99666ee09f69e063de0a04dbe119f0ed
Mario Manno <mail@manno.name>
2016-10-31 14:54:37 +0100

    Rails 5 disables autloading in production, use eager loading instead

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module MediaBackend
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     #
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
