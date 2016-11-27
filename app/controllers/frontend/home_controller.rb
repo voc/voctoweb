48085e81b299e9fdc3a0afc3a6b7158c597f065b
MaZderMind <git@mazdermind.de>
2016-11-27 19:50:01 +0100

    order conferences by recent event released_date

--- app/controllers/frontend/home_controller.rb
+++ app/controllers/frontend/home_controller.rb
@@ -1,11 +1,17 @@
 module Frontend
   class HomeController < FrontendController
+    helper_method :recent_events_for_conference
+
     def index
       @news = Frontend::News.recent(1).first()
       @recordings_count = Frontend::Recording.count
       @events_count = Frontend::Event.count
       @conferences_count = Frontend::Conference.count
 
+      @conference_limit = 3
+      @events_limit = 3
+
+      @recent_conferences = Frontend::Conference.with_recent_events().limit(@conference_limit)
 
       respond_to { |format| format.html }
     end
@@ -14,6 +20,12 @@ module Frontend
       respond_to do |format|
         format.html { render :page_not_found, status: 404 }
       end
+    end
+
+    private
+
+    def recent_events_for_conference(conference)
+      conference.events.order('release_date desc').limit(@events_limit)
     end
   end
 end
--- app/models/frontend/conference.rb
+++ app/models/frontend/conference.rb
@@ -3,6 +3,28 @@ module Frontend
     has_many :events, class_name: Frontend::Event
     has_many :recordings, through: :events
 
+    scope :with_events, ->() {
+      joins(:events)
+        .group('conferences.id')
+    }
+    scope :with_recent_events, ->() {
+      joins(:events)
+        .group('conferences.id')
+        .order('MAX(events.release_date) desc')
+    }
+    scope :with_events_newer, ->(date) {
+      joins(:events)
+        .group('conferences.id')
+        .having('MAX(events.release_date) > ?', date)
+        .order('MAX(events.release_date) desc')
+    }
+    scope :with_events_older, ->(date) {
+      joins(:events)
+        .group('conferences.id')
+        .having('MAX(events.release_date) < ?', date)
+        .order('MAX(events.release_date) desc')
+    }
+
     def mime_types
       return enum_for(:mime_types) unless block_given?
       recordings.pluck(:mime_type).uniq.map { |mime_type|
