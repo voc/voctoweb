e45b1e8134500d6e44a0da8f7656989fd4077a0b
Mario Manno <mail@manno.name>
2016-12-13 23:41:55 +0100

    Support multiple mail receivers

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -3,6 +3,6 @@ if Rails.env.production? && ENV['NOTIFY_RECEIVER'].present?
     email: {
       email_prefix: '[MEDIA] ',
       sender_address: ENV['NOTIFY_SENDER'],
-      exception_recipients: ENV['NOTIFY_RECEIVER']
+      exception_recipients: ENV['NOTIFY_RECEIVER'].split(/\s*,\s*/)
     }
 end
