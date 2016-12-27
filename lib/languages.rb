efbd65eb71161681f1846616a510873e9a20e32f
Mario Manno <mail@manno.name>
2016-12-27 16:41:15 +0100

    Add russian language

--- lib/languages.rb
+++ lib/languages.rb
@@ -6,6 +6,7 @@ class Languages
     'fra' => ['fr', 'French'],
     'spa' => ['es', 'Spanish'],
     'jpn' => ['ja', 'Japanese'],
+    'rus' => ['ru', 'Russian'],
     'orig' => ['', 'Original (different presenters using not the same languages, text not translated)'] # use only for subtiltes and not for audio or video recordings!
   }.freeze
 
@@ -17,7 +18,7 @@ class Languages
     def to_iso_639_1(lang)
       LANGUAGES[lang][0]
     end
-    
+
     def to_string(lang)
       LANGUAGES[lang][1]
     end
