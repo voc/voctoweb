843bf824f42cfa138f5b1654253e634b71c7d4cc
Mario Manno <mail@manno.name>
2017-01-03 15:56:51 +0100

    Store user agent and hashed IP address on recording views
    The IP is hashed together with a secret that's valid for 12 hours. This
    should in theory offer some protection against reversing it.
    The new identifier is also used in the existing throttle cache.

--- app/models/recording_view.rb
+++ app/models/recording_view.rb
@@ -1,4 +1,11 @@
 class RecordingView < ApplicationRecord
-  validates_presence_of :recording
+  validates :recording, presence: true
   belongs_to :recording
+
+  def identifier=(val)
+    secret = Rails.cache.fetch(:recording_view_secret, expires_in: 12.hours, race_condition_ttl: 10) do
+      SecureRandom.random_bytes(16)
+    end
+    self[:identifier] = Digest::SHA1.hexdigest(val + secret)
+  end
 end
