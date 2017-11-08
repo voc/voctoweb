cba3fadeee4c1f03ea958ee0a97ae7792c104c19
Tobias Gruetzmacher <tobias-git@23.gs>
2017-11-08 22:12:43 +0100

    Always stretch embeded player to full iframe size.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'ic
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [/\.(?:eot|otf|svg|ttf|woff|swf|svg|gif|png)/]
-Rails.application.config.assets.precompile += %w( mediaelement-and-player.js mediaelement-fix.js mirrorbrain-fix jquery mediaelementplayer )
+Rails.application.config.assets.precompile += %w( mediaelement-and-player.js mediaelement-fix.js mirrorbrain-fix jquery mediaelementplayer embed.css )
