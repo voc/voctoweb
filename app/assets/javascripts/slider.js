03c10cfd48e1f6720839db95a4029f512f81afb4
Mario Manno <mario.manno@gmail.com>
2017-11-13 14:21:32 +0100

    Avoid existing turbolink tricks for related slider

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,14 +1,16 @@
 $(function() {
   var initialPromotedHtml;
 
-  function init() {
+  function restorePromoted() {
     if(!initialPromotedHtml) {
       initialPromotedHtml = $('.promoted').html();
     }
     else {
       $('.promoted').html(initialPromotedHtml);
     }
+  }
 
+  function initSliders() {
     $('.promoted .slider').bxSlider({
       slideWidth: 200,
       minSlides: 1,
@@ -22,8 +24,25 @@ $(function() {
       autoHover: true, // Auto show will pause when mouse hovers over slider
       moveSlides: 1    // The number of slides to move on transition.
     });
+
+    $('.related .slider').bxSlider({
+      slideWidth: 200,
+      minSlides: 1,
+      maxSlides: 5,
+      slideMargin: 35,
+
+      captions: true,
+
+      auto: true,      // Slides will automatically transition
+      pause: 3000,     // The amount of time (in ms) between each auto transition
+      autoHover: true, // Auto show will pause when mouse hovers over slider
+      moveSlides: 1    // The number of slides to move on transition.
+    });
   }
-  $(document).on('turbolinks:load', init);
+
+  $(document).on('turbolinks:load', restorePromoted);
+  $(document).on('turbolinks:load', initSliders);
+
   $(document).on('ready resize turbolinks:load', function() {
     $('.bx-viewport').css({
       'left': '50%',
