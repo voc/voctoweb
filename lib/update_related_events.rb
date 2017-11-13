18445b791c49301b07de5d2e94b37a7f802f5806
Mario Manno <mario.manno@gmail.com>
2017-11-13 14:22:11 +0100

    Since recording view is based on 12 hour rotation 600 views by one identifier sounds unreasonable

--- lib/update_related_events.rb
+++ lib/update_related_events.rb
@@ -14,6 +14,47 @@ class UpdateRelatedEvents
 
   private
 
+  def related_by_views
+    views = RecordingView.connection.execute(%{
+        SELECT array_agg(DISTINCT recording_id) as recording_group
+          FROM recording_views
+         GROUP BY identifier, user_agent
+    })
+    views.values.map { |group| group.first[1..-2].split(/,/) }
+  end
+
+  def build_graph(related)
+    related.each { |events|
+      # identifier is valid for 12 hours only, how many presentations can one human watch?
+      next if events.size == 1 || events.size > 50
+
+      events.each { |event|
+        node = if @graph.key?(event)
+                 @graph[event]
+               else
+                 @graph[event] = Hash.new { |h, v| h[v] = 0 }
+               end
+
+        (events-[event]).each { |v| node[v] +=1 }
+      }
+    }
+
+    @graph.each do |_id, edges|
+      weights = edges.values
+      next if weights.count < 2
+      limit = percentile(weights, 0.95)
+      edges.delete_if { |_k, v| v < limit.to_i }
+    end
+  end
+
+  def percentile(values, percentile)
+    values_sorted = values.sort
+    k = (percentile * (values_sorted.length - 1) + 1).floor - 1
+    f = (percentile * (values_sorted.length - 1) + 1).modulo(1)
+
+    values_sorted[k] + (f * (values_sorted[k + 1] - values_sorted[k]))
+  end
+
   def update_related
     @graph.each do |id, edges|
       event = find_recording(id).event
@@ -34,43 +75,5 @@ class UpdateRelatedEvents
 
   def find_recording(recording_id)
     @recording_cache[recording_id] ||= Recording.find(recording_id)
-  end
-
-  def build_graph(related)
-    related.each { |events|
-      events.each { |event|
-        node = if @graph.key?(event)
-                 @graph[event]
-               else
-                 @graph[event] = Hash.new { |h, v| h[v] = 0 }
-               end
-
-        (events-[event]).each { |v| node[v] +=1 }
-      }
-    }
-
-    @graph.each do |_id, edges|
-      weights = edges.values
-      next if weights.count < 2
-      limit = percentile(weights, 0.95)
-      edges.delete_if { |_k, v| v < limit.to_i }
-    end
-  end
-
-  def related_by_views
-    views = RecordingView.connection.execute(%{
-        SELECT array_agg(DISTINCT recording_id) as recording_group
-          FROM recording_views
-         GROUP BY identifier, user_agent
-    })
-    views.values.map { |group| group.first[1..-2].split(/,/) }
-  end
-
-  def percentile(values, percentile)
-    values_sorted = values.sort
-    k = (percentile * (values_sorted.length - 1) + 1).floor - 1
-    f = (percentile * (values_sorted.length - 1) + 1).modulo(1)
-
-    values_sorted[k] + (f * (values_sorted[k + 1] - values_sorted[k]))
   end
 end
