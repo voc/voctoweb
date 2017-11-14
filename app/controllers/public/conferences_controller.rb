9ae2ab15d591d628f711d8d545c5e9ac3b0cd455
Andreas Hubel <andi@saerdnaer.de>
2017-11-14 15:10:33 +0100

    public API: add /public/conferences/:acronym
    implements #258

--- app/controllers/public/conferences_controller.rb
+++ app/controllers/public/conferences_controller.rb
@@ -3,5 +3,18 @@ module Public
     include ApiErrorResponses
     respond_to :json
     actions :index, :show
+    
+    # GET /public/conferences/54
+    # GET /public/conferences/54.json
+    # GET /public/conferences/31c3
+    # GET /public/conferences/31c3.json
+    def show
+      if params[:id] =~ /\A[0-9]+\z/
+        @conference = Conference.find(params[:id])
+      else
+        @conference = Conference.find_by(acronym: params[:id])
+      end
+      fail ActiveRecord::RecordNotFound unless @conference
+    end
   end
 end
