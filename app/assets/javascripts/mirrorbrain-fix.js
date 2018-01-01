60d2aab0d8c7d567205450242cbe635dba5f21c5
Andreas Hubel <andi@saerdnaer.de>
2018-01-01 18:13:12 +0100

    fix oembed

--- app/assets/javascripts/mirrorbrain-fix.js
+++ app/assets/javascripts/mirrorbrain-fix.js
@@ -13,46 +13,5 @@ var MirrorbrainFix = {
         cb(mirror);
       }
     });
-  },
-
-  setupPlayer: function() {
-    var stamp = window.location.hash.split('#t=')[1] || window.location.hash.split('&t=')[1],
-        $video = $('video'),
-        promises = [];
-
-    $('video source').each(function() {
-      var $source = $(this);
-      // Prop always presents the fully resolved URL
-      promises.push(
-        MirrorbrainFix.selectMirror($source.prop('src'), function(mirror) {
-        $source.attr('src', mirror);
-      })
-      );
-    });
-
-    $.when.apply($, promises).done(function() {
-      $('video').mediaelementplayer({
-        usePluginFullScreen: true,
-        enableAutosize: true,
-        stretching: 'responsive',
-        features: ['skipback', 'playpause', 'jumpforward', 'progress', 'current', 'duration', 'tracks', 'volume', 'speed', 'sourcechooser', 'fullscreen', 'postroll'],
-        skipBackInterval: 15,
-        success: function (mediaElement) {
-          mediaElement.addEventListener('canplay', function () {
-            if(stamp) {
-              mediaElement.setCurrentTime(stamp);
-              stamp = null;
-            }
-          });
-          mediaElement.addEventListener('playing', function () {
-            $.post("/public/recordings/count", {event_id: $video.data('id'), src: mediaElement.src});
-          }, false);
-          mediaElement.addEventListener('pause', function() {
-            var hash = '#t='+Math.round(mediaElement.currentTime);
-            window.location.replaceHash(hash);
-          }, false);
-        }
-      });
-    });
   }
 }
