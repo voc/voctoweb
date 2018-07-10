d602d3b86196d56eb75d481cece0f002e9783055
Mario Manno <mario.manno@gmail.com>
2018-07-11 01:20:28 +0200

    Remove tinymce registration from activeadmin config

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -128,10 +128,6 @@ ActiveAdmin.setup do |config|
   #
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
   #   config.register_stylesheet 'my_print_stylesheet.css', :media => :print
-  #
-  # To load a javascript file:
-  #   config.register_javascript 'my_javascript.js'
-  config.register_javascript 'tinymce.js'
 
 
   # == CSV options
