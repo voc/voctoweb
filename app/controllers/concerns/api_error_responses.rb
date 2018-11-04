3b271c9c24c975adea674ff3e2c7d4a1f62f6aff
Andreas Hubel <andi@saerdnaer.de>
2018-11-04 22:22:20 +0100

    API: do not return 500 error's as HTML

--- app/controllers/concerns/api_error_responses.rb
+++ app/controllers/concerns/api_error_responses.rb
@@ -4,6 +4,7 @@ module ApiErrorResponses
   included do
     include ActionController::MimeResponds
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from StandardError, with: :error
     before_action { |controller| set_header(controller) }
 
   end
@@ -16,6 +17,11 @@ module ApiErrorResponses
       format.xml { render xml: { message: 'not found', error: error.message }, status: :not_found }
     end
   end
+
+  def error(error)
+    respond_to do |format|
+      format.json { render json: { message: 'error', error: error.message }, status: 500 }
+      format.xml { render xml: { message: 'error', error: error.message }, status: 500 }
 
   def set_header(controller)
     unless controller.response.headers.key?('Content-Type')
