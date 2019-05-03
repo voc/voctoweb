c246b677b71f4cca8f6d8373380be56d6843d886
Andreas Hubel <andi@saerdnaer.de>
2019-05-04 00:29:54 +0200

    hopefully fix problems with ruby 2.4.0

--- app/graphql/types/date_time_type.rb
+++ app/graphql/types/date_time_type.rb
@@ -2,7 +2,12 @@ module Types
   class DateTimeType < Types::BaseScalar
     graphql_name 'DateTime'
   
-    coerce_input ->(value, _ctx) { Time.zone.parse(value) }
-    coerce_result ->(value, _ctx) { value.utc.iso8601 }
+    def self.coerce_input(input_value, context)
+      Time.zone.parse(input_value)
+    end
+  
+    def self.coerce_result(ruby_value, context)
+      ruby_value.value.utc.iso8601
+    end
   end
 end
