d23100062287486a95e969ad596e0e57ed9ba8d3
Andreas Hubel <andi@saerdnaer.de>
2019-05-07 01:15:56 +0200

    set response header to json, when not already set

--- app/controllers/concerns/api_error_responses.rb
+++ app/controllers/concerns/api_error_responses.rb
@@ -4,6 +4,8 @@ module ApiErrorResponses
   included do
     include ActionController::MimeResponds
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    after_action { |controller| set_header(controller) }
+
   end
 
   private
@@ -12,6 +14,12 @@ module ApiErrorResponses
     respond_to do |format|
       format.json { render json: { message: 'not found', error: error.message }, status: :not_found }
       format.xml { render xml: { message: 'not found', error: error.message }, status: :not_found }
+    end
+  end
+
+  def set_header(controller)
+    unless controller.response.headers.key?('Content-Type')
+      controller.response['Content-Type'] = 'application/json'
     end
   end
 end
