1dae3df131a1426a5ad531eee2e56e8e7341d377
Andreas Hubel <andi@saerdnaer.de>
2019-12-23 22:51:04 +0100

    make test output more consistent

--- test/controllers/frontend/feeds_controller_test.rb
+++ test/controllers/frontend/feeds_controller_test.rb
@@ -69,13 +69,13 @@ module Frontend
 
     test 'round_to_next_quarter_hour' do
       time = @controller.send(:round_to_quarter_hour, Time.parse('2017-02-12 14:21:42 +00000'))
-      assert_equal time, Time.parse('2017-02-12 14:15:00 +0000')
+      assert_equal Time.parse('2017-02-12 14:15:00 +0000'), time
 
       time = @controller.send(:round_to_quarter_hour, Time.parse('2017-02-12 17:38:10 +00000'))
-      assert_equal time, Time.parse('2017-02-12 17:30:00 +0000')
+      assert_equal Time.parse('2017-02-12 17:30:00 +0000'), time
 
       time = @controller.send(:round_to_quarter_hour, Time.parse('2017-02-12 17:46:10 +00000'))
-      assert_equal time, Time.parse('2017-02-12 17:45:00 +0000')
+      assert_equal Time.parse('2017-02-12 17:45:00 +0000'), time
     end
   end
 end
