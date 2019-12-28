7de06139eb224a56e4d8a24f74de55a75893908d
Andreas Hubel <andi@saerdnaer.de>
2019-12-28 20:30:46 +0100

    #421 fix relive player when opened via turbolinks

--- app/assets/javascripts/relive-seek.js
+++ app/assets/javascripts/relive-seek.js
@@ -12,53 +12,61 @@ $(function() {
     return result;
   }
 
-  var $relivePlayer = $('.relive-player .video-wrap');
-  if($relivePlayer.length > 0) {
-    var sprites = [];
+  function tryInitRelivePlayer() {
+    var $relivePlayer = $(".relive-player .video-wrap");
+    if ($relivePlayer.length > 0) {
+      var sprites = [];
 
-		if($relivePlayer.data("sprites")) {
-			sprites = ClapprThumbnailsPlugin.buildSpriteConfig(
-				$relivePlayer.data("sprites"),
-				$relivePlayer.data("sprites-n"),
-				160, 90,
-				$relivePlayer.data("sprites-cols"),
-				$relivePlayer.data("sprites-interval")
-			);
-		}
+      if ($relivePlayer.data("sprites")) {
+        sprites = ClapprThumbnailsPlugin.buildSpriteConfig(
+          $relivePlayer.data("sprites"),
+          $relivePlayer.data("sprites-n"),
+          160,
+          90,
+          $relivePlayer.data("sprites-cols"),
+          $relivePlayer.data("sprites-interval")
+        );
+      }
 
-		var player = new Clappr.Player({
-			baseUrl: 'assets/clapprio/',
-			plugins: {
-				core: [ClapprThumbnailsPlugin, PlaybackRatePlugin]
-			},
+      var player = new Clappr.Player({
+        baseUrl: "assets/clapprio/",
+        plugins: {
+          core: [ClapprThumbnailsPlugin, PlaybackRatePlugin]
+        },
 
-			source: $relivePlayer.data('m3u8'),
-			height: $relivePlayer.data('height'),
-			width: $relivePlayer.data('width'),
-			autoPlay: true,
-			scrubThumbnails: {
-				backdropHeight: 64,
-				spotlightHeight: 84,
-				thumbs: sprites
-			},
-			events: {
-				onReady: function() {
-					var playback = player.core.getCurrentContainer().playback;
-					var params = deserialize(location.href)
+        source: $relivePlayer.data("m3u8"),
+        height: $relivePlayer.data("height"),
+        width: $relivePlayer.data("width"),
+        autoPlay: true,
+        scrubThumbnails: {
+          backdropHeight: 64,
+          spotlightHeight: 84,
+          thumbs: sprites
+        },
+        events: {
+          onReady: function() {
+            var playback = player.core.getCurrentContainer().playback;
+            var params = deserialize(location.href);
 
-					playback.once(Clappr.Events.PLAYBACK_PLAY, function() {
-						var seek = parseFloat(params.t);
-						if (!isNaN(seek)) {
-							player.seek(seek);
-						} else if (playback.getPlaybackType() == 'vod') {
-							// skip forward to scheduled beginning of the talk at ~ 0:14:30  (30 sec offset, if speaker starts on time)
-							player.seek(14 * 60 + 30);
-						}
-					});
-				}
-			}
-		});
-
-    player.attachTo($relivePlayer.get(0));
+            playback.once(Clappr.Events.PLAYBACK_PLAY, function() {
+              var seek = parseFloat(params.t);
+              if (!isNaN(seek)) {
+                player.seek(seek);
+              } else if (playback.getPlaybackType() == "vod") {
+                // skip forward to scheduled beginning of the talk at ~ 0:14:30  (30 sec offset, if speaker starts on time)
+                player.seek(14 * 60 + 30);
+              }
+            });
+          }
+        }
+      });
+      player.attachTo($relivePlayer.get(0));
+    }
   }
+  tryInitRelivePlayer();
+
+  $(document).on('turbolinks:load', function() {
+    tryInitRelivePlayer();
+  });
+
 });
