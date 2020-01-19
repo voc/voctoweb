ee5477fc8e56f967a6d038a276ac41a329b6992d
Andreas Hubel <andi@saerdnaer.de>
2020-01-19 14:58:54 +0100

    ran bundle update and fixed build

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+
+//= link graphiql/rails/application.css
+//= link graphiql/rails/application.js
