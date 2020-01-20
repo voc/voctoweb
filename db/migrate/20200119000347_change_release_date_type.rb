ea212d871b121d83ab3b9bbc167137951c7dbe3d
Andreas Hubel <andi@saerdnaer.de>
2020-01-20 01:04:48 +0100

    fix migration

--- db/migrate/20200119000347_change_release_date_type.rb
+++ db/migrate/20200119000347_change_release_date_type.rb
@@ -1,6 +1,6 @@
 class ChangeReleaseDateType < ActiveRecord::Migration[5.1]
   def change
     change_column :events, :release_date, :datetime;
-    change_column :conference, :event_last_released_at, :datetime;
+    change_column :conferences, :event_last_released_at, :datetime;
   end
 end
