55dffb17a6153024939892ea781be2301f09d9f6
Andreas Hubel <andi@saerdnaer.de>
2020-01-22 22:49:36 +0100

    fix tests?

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-
-//= link graphiql/rails/application.css
-//= link graphiql/rails/application.js
