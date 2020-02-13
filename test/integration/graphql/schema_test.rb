581e1373c819a521d44d81bd755e485544d7387e
Andreas Hubel <andi@saerdnaer.de>
2020-02-13 22:46:39 +0100

    add schema to repo

--- test/integration/graphql/schema_test.rb
+++ test/integration/graphql/schema_test.rb
@@ -4,6 +4,6 @@ class SchemaTest < ActiveSupport::TestCase
   def test_printout_is_up_to_date
     current_defn = MediaBackendSchema.to_definition
     printout_defn = File.read(Rails.root.join("app/graphql/schema.graphql"))
-    assert_equal(current_defn, printout_defn, "Update the printed schema with `bundle exec rake graphql:dump_schema`")
+    assert_equal(current_defn, printout_defn, "Update the printed schema with `bundle exec rake graphql:schema:dump`")
   end
 end
