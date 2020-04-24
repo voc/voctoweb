d142457d9af7f859c987555574dc1e37689b019f
Michael F. Schönitzer <michael@schoenitzer.de>
2020-04-25 00:12:53 +0200

    Add subpages to 'Events' to filter by year
    This commit enhances the "Popular Events" page in two ways:
    - Allow to limit the shown events by year of the event
    - Allow to browse through multiple pages

--- app/controllers/frontend/popular_controller.rb
+++ app/controllers/frontend/popular_controller.rb
@@ -1,7 +1,27 @@
 module Frontend
   class PopularController < FrontendController
+    PAGE_SIZE = 20
+
     def index
-      @events = Frontend::Event.order('view_count DESC').limit(20).includes(:conference)
+      @year = params[:year].to_i.to_s
+      @page = params[:page].to_i || 0
+      @firstyear = Frontend::Event
+        .select('date_part(\'year\',min(date)) as firstyear, -1 AS guid')
+        .take['firstyear'].to_i
+      if @year == "0"
+        @events = Frontend::Event
+          .order('view_count DESC')
+          .limit(PAGE_SIZE)
+          .offset(@page * PAGE_SIZE)
+          .includes(:conference)
+      else
+        @events = Frontend::Event
+          .where('date_part(\'year\', date) = ?', @year )
+          .order('view_count DESC')
+          .limit(PAGE_SIZE)
+          .offset(@page * PAGE_SIZE)
+          .includes(:conference)
+      end
 
       respond_to { |format| format.html }
     end
