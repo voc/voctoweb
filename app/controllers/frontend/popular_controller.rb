2c14ae913e00c7a8eb4a84b855a1fe2a73109d4a
Andreas Hubel <andi@saerdnaer.de>
2020-05-03 19:43:30 +0200

    cleanup

--- app/controllers/frontend/popular_controller.rb
+++ app/controllers/frontend/popular_controller.rb
@@ -3,24 +3,20 @@ module Frontend
     PAGE_SIZE = 20
 
     def index
-      @year = params[:year].to_i.to_s
+      @year = params[:year].to_i
       @page = params[:page].to_i || 0
-      @firstyear = Frontend::Event
-        .select('date_part(\'year\',min(date)) as firstyear, -1 AS guid')
-        .take['firstyear'].to_i
-      if @year == "0"
-        @events = Frontend::Event
-          .order('view_count DESC')
-          .limit(PAGE_SIZE)
-          .offset(@page * PAGE_SIZE)
-          .includes(:conference)
+      @firstyear = Frontend::Event.order('date ASC').limit(1).first.date.year
+      if @year === 0
+        @events = Frontend::Event.order('view_count DESC')
+                                 .limit(PAGE_SIZE)
+                                 .offset(@page * PAGE_SIZE)
+                                 .includes(:conference)
       else
-        @events = Frontend::Event
-          .where('date_part(\'year\', date) = ?', @year )
-          .order('view_count DESC')
-          .limit(PAGE_SIZE)
-          .offset(@page * PAGE_SIZE)
-          .includes(:conference)
+        @events = Frontend::Event.where('date between ? and ?', "#{@year}-01-01", "#{@year}-12-31")
+                                 .order('view_count DESC')
+                                 .limit(PAGE_SIZE)
+                                 .offset(@page * PAGE_SIZE)
+                                 .includes(:conference)
       end
 
       respond_to { |format| format.html }
