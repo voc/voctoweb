7181bf978818967a634c44dbc494c98690d6b9d2
Andreas Hubel <andi@saerdnaer.de>
2021-02-21 15:23:44 +0100

    settings: add custom_css to allow visual seperation of instances

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module MediaBackend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.custom_css = nil
   end
 end
