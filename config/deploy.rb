a92fc6f0db7801e0c03f7f659e1fb298f5130c1b
Andreas Hubel <andi@saerdnaer.de>
2022-05-19 23:12:36 +0200

    try to fix deployment when puma is not running

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
+      before 'deploy:restart' #, 'puma:start'
       invoke 'deploy'
     end
   end
