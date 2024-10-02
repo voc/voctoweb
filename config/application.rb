7830960436951cf20c74420971bcbdd18308868a
Mario Manno <mm@koeln.ccc.de>
2024-10-02 15:23:34 +0200

    Switch to 7.0 framework defaults
    Tests pass :shrug:

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Dotenv::Rails.load
 module MediaBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
