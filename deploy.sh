2874b92dd0b7a2f961230dc7b90c63e61f7c095f
Qb <qb@qbit.moe>
2024-12-28 13:25:36 +0100

    Implement additional fields for card integration

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+export CAP_REPO=https://github.com/voc/voctoweb.git
+export CAP_BRANCH=main
+export CAP_USER=media
+
+export CAP_HOST=app.media.ccc.de
+export CAP_PORT=22
+
 bundle exec cap production deploy
