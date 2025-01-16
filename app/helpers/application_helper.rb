551a7c8a2712554e2d550c3fb9a1e458a54830c4
Daniel Molkentin <daniel@molkentin.de>
2025-01-16 23:55:08 +0100

    make larger viewcounts readable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require "active_support/number_helper"
+
 module ApplicationHelper
   def line_break_filename(filename)
     if filename.present?
@@ -5,5 +7,15 @@ module ApplicationHelper
     else
       ''
     end
+  end
+  def human_readable_views_count(count)
+    if count < 1000 
+      "#{count}"
+    else
+      "#{(count / 1000.0).round(1)}k"
+    end
+  end
+  def delimited_views_count(count)
+    ActiveSupport::NumberHelper.number_to_delimited(count)
   end
 end
