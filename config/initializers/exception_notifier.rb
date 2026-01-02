6fef57ca148b7a27e6816521ccd94b1d21322b42
Mario Manno <mario.manno@gmail.com>
2026-01-02 17:38:38 +0100

    Update exception notifier to ignore empty multipart errors
    Previously, only IP spoofing attacks were ignored by the notifier. Now,
    invalid multipart request parameter errors are excluded from
    notifications to reduce noise and focus on actionable exceptions.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,6 +1,8 @@
 if Rails.env.production? && ENV['NOTIFY_RECEIVER'].present?
   Rails.application.config.middleware.use ExceptionNotification::Rack,
-    ignore_if: ->(env, exception) { exception.message =~ /^IP spoofing attack/ },
+    # See https://github.com/kmcphillips/exception_notification?tab=readme-ov-file#ignore-exceptions
+    #ignore_exceptions: ['ActionView::TemplateError'] + ExceptionNotifier.ignored_exceptions,
+    ignore_if: ->(env, exception) { exception.message =~ /^Invalid request parameters: Rack::Multipart::EmptyContentError/ },
     email: {
       email_prefix: '[MEDIA] ',
       sender_address: ENV['NOTIFY_SENDER'],
