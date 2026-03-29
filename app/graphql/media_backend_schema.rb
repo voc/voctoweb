09bccb9f1b2b17216563e74bc0415ed3a4ba1792
Andi <andi@saerdnaer.de>
2026-03-30 00:01:38 +0200

    chore: change graphql query max_depth

--- app/graphql/media_backend_schema.rb
+++ app/graphql/media_backend_schema.rb
@@ -2,7 +2,7 @@ class MediaBackendSchema < GraphQL::Schema
   include ApolloFederation::Schema
   use ApolloFederation::Tracing
 
-  max_depth 13
+  max_depth 17
   query(Types::QueryType)
   #mutation(Types::MutationType)
 end
